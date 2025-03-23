157c1cb6e4a9b610d90fb22f5814641334470e81
mona-abdelmohsen <monaabdelmohsen101214@gmail.com>
2025-03-23 12:16:13 +0200

    update google auth after use hard delete for uers

--- YourBook-BE/app/Http/Controllers/API/GoogleTokenController.php
+++ YourBook-BE/app/Http/Controllers/API/GoogleTokenController.php
@@ -14,43 +14,107 @@ class GoogleTokenController extends Controller
 {
     use ApiResponse;
 
-    public function verifyGoogleToken(Request $request)
+    // public function verifyGoogleToken(Request $request)
+    // {
+    //     $idToken = $request->input('id_token');
+
+    //     if (!$idToken) {
+    //         return $this->error(Lang::has('validation.custom.token_missing') ? Lang::get('validation.custom.token_missing') : 'Token is missing', null, self::$responseCode::HTTP_BAD_REQUEST);
+    //     }
+
+    //     try {
+    //         $googleService = json_decode(file_get_contents(config_path('google-service.json')), true);
+    //         $response = Http::get('https://oauth2.googleapis.com/tokeninfo', ['id_token' => $idToken]);
+
+    //         if ($response->failed()) {
+    //             return $this->error(Lang::has('validation.custom.invalid_token') ? Lang::get('validation.custom.invalid_token') : 'Invalid token', null, self::$responseCode::HTTP_UNAUTHORIZED);
+    //         }
+
+    //         $payload = $response->json();
+
+    //         if ($payload['iss'] !== 'https://accounts.google.com') {
+    //             return $this->error(Lang::has('validation.custom.invalid_issuer') ? Lang::get('validation.custom.invalid_issuer') : 'Invalid token issuer', null, self::$responseCode::HTTP_UNAUTHORIZED);
+    //         }
+
+    //         if ($payload['exp'] < time()) {
+    //             return $this->error(Lang::has('validation.custom.token_expired') ? Lang::get('validation.custom.token_expired') : 'Token has expired', null, self::$responseCode::HTTP_UNAUTHORIZED);
+    //         }
+            
+    //         $user = User::withTrashed()->where('email', 'like', '%'.mb_strtolower($payload['email']).'%')->first();
+
+    //         if($user){
+                
+    //             if ($user->trashed()) {
+    //                 $user->restore();
+    //             }
+
+    //             $user->update([
+    //                 'email_verified_at' => now(),
+    //                 'google_id' => $payload['sub'],
+    //             ]);
+                
+    //         }else{
+    //             $user = User::create([
+    //                 'name' => $payload['name'] ?? (Lang::has('validation.custom.default_user_name') ? Lang::get('validation.custom.default_user_name') : 'User'),    
+    //                 'email' => $payload['email'],
+    //                 'email_verified_at' => now(),
+    //                 'google_id' => $payload['sub'],
+    //             ]);
+    //         }
+
+    //         $tokenResult = $user->createToken('GoogleLoginToken');
+    //         $accessToken = $tokenResult->accessToken;
+    //         $refreshToken = $tokenResult->token->refresh_token ?? null;
+
+    //         return response()->json([
+    //             'token_type' => 'Bearer',
+    //             'expires_in' => 1296000,
+    //             'access_token' => $accessToken,
+    //             'refresh_token' => $refreshToken,
+    //         ], self::$responseCode::HTTP_OK);
+    //     } catch (\Exception $e) {
+    //         return $this->error($e->getMessage(), null, self::$responseCode::HTTP_INTERNAL_SERVER_ERROR);
+    //     }
+    // }
+    
+    
+    
+    // updated after use harddelete for users
+     public function verifyGoogleToken(Request $request)
     {
         $idToken = $request->input('id_token');
 
         if (!$idToken) {
-            return $this->error(Lang::has('validation.custom.token_missing') ? Lang::get('validation.custom.token_missing') : 'Token is missing', null, self::$responseCode::HTTP_BAD_REQUEST);
+            return $this->error(Lang::get('validation.custom.token_missing', [], 'Token is missing'), null, self::$responseCode::HTTP_BAD_REQUEST);
         }
 
         try {
-            $googleService = json_decode(file_get_contents(config_path('google-service.json')), true);
             $response = Http::get('https://oauth2.googleapis.com/tokeninfo', ['id_token' => $idToken]);
 
             if ($response->failed()) {
-                return $this->error(Lang::has('validation.custom.invalid_token') ? Lang::get('validation.custom.invalid_token') : 'Invalid token', null, self::$responseCode::HTTP_UNAUTHORIZED);
+                return $this->error(Lang::get('validation.custom.invalid_token', [], 'Invalid token'), null, self::$responseCode::HTTP_UNAUTHORIZED);
             }
 
             $payload = $response->json();
 
             if ($payload['iss'] !== 'https://accounts.google.com') {
-                return $this->error(Lang::has('validation.custom.invalid_issuer') ? Lang::get('validation.custom.invalid_issuer') : 'Invalid token issuer', null, self::$responseCode::HTTP_UNAUTHORIZED);
+                return $this->error(Lang::get('validation.custom.invalid_issuer', [], 'Invalid token issuer'), null, self::$responseCode::HTTP_UNAUTHORIZED);
             }
 
             if ($payload['exp'] < time()) {
-                return $this->error(Lang::has('validation.custom.token_expired') ? Lang::get('validation.custom.token_expired') : 'Token has expired', null, self::$responseCode::HTTP_UNAUTHORIZED);
+                return $this->error(Lang::get('validation.custom.token_expired', [], 'Token has expired'), null, self::$responseCode::HTTP_UNAUTHORIZED);
             }
             
-            $user = User::withTrashed()->where('email', 'like', '%'.mb_strtolower($payload['email']).'%')->first();
+            $user = User::where('email', 'like', '%' . mb_strtolower($payload['email']) . '%')->first();
 
-            if($user){
+            if ($user) {
                 $user->update([
                     'email_verified_at' => now(),
                     'google_id' => $payload['sub'],
-                    'deleted_at' => null,
                 ]);
-            }else{
+            } else {
                 $user = User::create([
-                    'name' => $payload['name'] ?? (Lang::has('validation.custom.default_user_name') ? Lang::get('validation.custom.default_user_name') : 'User'),    
+                    'name' => $payload['name'] ?? Lang::get('validation.custom.default_user_name', [], 'User'),    
                     'email' => $payload['email'],
                     'email_verified_at' => now(),
                     'google_id' => $payload['sub'],
@@ -72,4 +136,3 @@ class GoogleTokenController extends Controller
         }
     }
 }
-
